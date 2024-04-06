74b5ac06d5f7ebde788fe536c6a8ecbfbd474ac5
alliyaa <alliyaahmad#@gmail.com>
2024-04-06 14:22:48 +0300

    added logo and styling on home page

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ homeButton.addEventListener('click',  () =>
 function createSecondPage()
 {
     const secondPageDiv = document.createElement('div');
+    secondPageDiv.classList.add('secondPageDiv');
+
+    const carButton = document.createElement('button');
+    carButton.textContent = 'Car';
+    secondPageDiv.appendChild(carButton);
+
+    
+    const busButton = document.createElement('button');
+    busButton.textContent = 'Bus';
+    secondPageDiv.appendChild(busButton);
+    
+    
+    document.body.appendChild(secondPageDiv); 
+
 }
