8d6f905e96636488dc63e077bdd8678e10db7043
alliyaa <alliyaahmad#@gmail.com>
2024-04-07 10:33:48 +0300

    added pictures for car/bus/train and layout for the second page

--- script.js
+++ script.js
@@ -11,18 +11,30 @@ function createSecondPage()
 {
     const secondPageDiv = document.createElement('div');
     secondPageDiv.classList.add('secondPageDiv');
+    secondPageDiv.textContent="Choose Your Mode of Transportation";
 
+    const carImg = document.createElement('img');
+    carImg.classList.add('carImg');
+    carImg.src = 'Car.png';
+    secondPageDiv.appendChild(carImg);
     const carButton = document.createElement('button');
     carButton.textContent = 'Car';
     secondPageDiv.appendChild(carButton);
     carButton.classList.add('car');
 
-    
+    const busImg = document.createElement('img');
+    busImg.classList.add('busImg');
+    busImg.src = 'bus.png';
+    secondPageDiv.appendChild(busImg);
     const busButton = document.createElement('button');
     busButton.textContent = 'Bus';
     secondPageDiv.appendChild(busButton);
     busButton.classList.add('bus');
-        
+
+    const trainImg = document.createElement('img');
+    trainImg.classList.add('trainImg');
+    trainImg.src = 'Train.png';
+    secondPageDiv.appendChild(trainImg);
     const trainButton = document.createElement('button');
     trainButton.textContent = 'Train';
     secondPageDiv.appendChild(trainButton);
